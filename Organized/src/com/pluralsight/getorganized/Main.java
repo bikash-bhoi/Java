d16418118336af77b07e44192a40cf43ed127f3d
bikash-bhoi <bhoi.bikash@gmail.com>
2017-05-15 13:35:21 +0530

    Add files via upload

--- Organized/src/com/pluralsight/getorganized/Main.java
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,6 @@ package com.pluralsight.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi Hello");
     }
 }
