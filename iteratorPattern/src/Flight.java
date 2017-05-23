3c039e694d8c1fa45d3ae64341d569f11f00c81b
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2017-05-23 17:18:13 +0530

    Update Flight.java

--- iteratorPattern/src/Flight.java
+++ iteratorPattern/src/Flight.java
@@ -1,45 +1,79 @@
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by Biks on 5/22/2017.
  */
-public class Flight implements Iterable<Person> {
+public class Flight {
 
-    private CrewMember[] crew;
-    private Passenger[] roster;
+    private ArrayList<CrewMember> crew;
+    private ArrayList<Passenger> roster;
 
-    public Flight(CrewMember[] crew, Passenger[] roster){
-        this.crew = crew;
-        this.roster = roster;
+    public Flight(){
+        this.crew = new ArrayList<CrewMember>();
+        this.roster = new ArrayList<Passenger>();
     }
 
     public Iterator<Person> iterator(){
         return new FlightIterator(crew, roster);}
 
+    public void addCrew(String name){
+        CrewMember newCrew = new CrewMember(name);
+        if (!(crew.add(newCrew)))
+            System.out.println("Error Adding Crew");
+    }
+    public void addPassenger(String name){
+        Passenger newPassenger = new Passenger(name);
+        if (!roster.add(newPassenger))
+            System.out.println("Error Adding Passenger");
+    }
+
+    public void printPassengers() {
+        Iterator<Person> persons = this.iterator();
+        int count = 1;
+        System.out.println("********************************");
+        while (persons.hasNext()) {
+            Person p = persons.next();
+            System.out.println(count++ + ". " + p.getName());
+        }
+        System.out.println("********************************");
+    }
+
+    public void removeLastPassenger(){
+        Iterator<Person> persons = this.iterator();
+        persons.remove();
+    }
+
+    //Private class to Iterate
     private class FlightIterator implements Iterator<Person> {
 
-        private CrewMember[] crew;
-        private Passenger[] roster;
+        private ArrayList<CrewMember> crew;
+        private ArrayList<Passenger> roster;
         private int index = 0;
 
-        public FlightIterator(CrewMember[] crew, Passenger[] roster){
+        public FlightIterator(ArrayList<CrewMember> crew, ArrayList<Passenger> roster){
             this.crew = crew;
             this.roster = roster;
         }
 
         public boolean hasNext() {
-            return index < crew.length + roster.length;
+            return index < crew.size() + roster.size();
         }
 
 
         public Person next() {
-            Person p = (index < crew.length) ?
-                    crew[index] : roster[index - crew.length];
-            index++;
+            Person p = (index < crew.size()) ?
+                    crew.get(index++) : roster.get(index++ - crew.size());
             return p;
         }
+
+        @Override
+        public void remove() {
+            if (roster.size() == 0)
+                System.out.println("****No Passengers to remove****");
+            else{
+
+                roster.remove(roster.size() - 1);
+            }
+        }
     }
-
-
 }
-
